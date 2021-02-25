a3c2fc9b379c30912d743aa00831f51eca765c11
Diego Barbosa <diego1279.dr@gmail.com>
2021-02-25 10:21:22 -0300

    alteração

--- ProAgil.Repositorio/ProAgilRepositorio.cs
+++ ProAgil.Repositorio/ProAgilRepositorio.cs
@@ -8,134 +8,197 @@ namespace ProAgil.Repositorio
 {
     public class ProAgilRepositorio : IProAgilRepositorio
     {
-        private readonly ProAgilContext _context;
 
-        public ProAgilRepositorio(ProAgilContext context)
-        {
-            _context = context;
-            _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-        }
+
+        
 
         //Geral
         public void Add<T>(T entity) where T : class
         {
-            _context.Add(entity);
+            throw new System.NotImplementedException();
         }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
         public void Update<T>(T entity) where T : class
         {
-            _context.Update(entity);
+            throw new System.NotImplementedException();
         }
+
         public void Delete<T>(T entity) where T : class
         {
-            _context.Remove(entity);
+            throw new System.NotImplementedException();
         }
-        public void DeleteRange<T>(T[] entityArray) where T : class
+
+        public void DeleteRange<T>(T[] entity) where T : class
         {
-            _context.RemoveRange(entityArray);
+            throw new System.NotImplementedException();
         }
-        public async Task<bool> SaveChangesAsync()
-        {
-            return (await _context.SaveChangesAsync()) > 0;
-        }
+
 
         //Evento
-        public async Task<Evento[]> GetAllEventoAsync(bool IncludeUsers = false)
+        public Task<Evento[]> GetAllEventoAsync(bool IncludeUsers)
         {
-            IQueryable<Evento> query = _context.Eventos
-                .Include(c => c.Lotes)
-                .Include(c => c.RedesSociais);
-
-            if (IncludeUsers)
-            {
-                query = query
-                    .Include(ue => ue.UsersEventos)
-                    .ThenInclude(u => u.User);
-            }
-
-            query = query.AsNoTracking()
-                        .OrderBy(c => c.Id);
-
-            return await query.ToArrayAsync();
+            throw new System.NotImplementedException();
         }
 
-        public async Task<Evento> GetEventoAsyncById(int EventoId, bool IncludeUsers)
+        public Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool IncludeUsers)
         {
-           IQueryable<Evento> query = _context.Eventos
-                .Include(c => c.Lotes)
-                .Include(c => c.RedesSociais);
-
-            if(IncludeUsers)
-            {
-                query = query
-                        .Include(ue => ue.UsersEventos)
-                        .ThenInclude(u => u.User);
-            }
-
-            query = query.AsNoTracking()
-            .OrderBy(c => c.Id)
-                .Where(c => c.Id == EventoId);
-            
-            return await query.FirstOrDefaultAsync();
+            throw new System.NotImplementedException();
         }
 
-        public async Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includeUsers)
+        public Task<Evento> GetEventoAsyncById(int EventoId, bool IncludeUsers)
         {
-            IQueryable<Evento> query = _context.Eventos
-                .Include(c => c.Lotes)
-                .Include(c => c.RedesSociais);
-
-            if (includeUsers)
-            {
-                query = query
-                    .Include(ue => ue.UsersEventos)
-                    .ThenInclude(p => p.User);
-            }
-
-            query = query.AsNoTracking()
-                        .OrderByDescending(c => c.DataEvento)
-                        .Where(c => c.Tema.ToLower().Contains(tema.ToLower()));
-
-            return await query.ToArrayAsync();
+            throw new System.NotImplementedException();
         }
 
-        //Palestrante
-        public async Task<User> GetAllUserAsync(int UserId, bool IncludeEventos = false)
+
+        //User
+        public Task<User> GetAllUserAsync(int UserId, bool IncludeEventos)
         {
-            IQueryable<User> query = _context.Users
-                .Include(c => c.RedesSociais);
-
-            if(IncludeEventos)
-            {
-                query = query
-                        .Include(ue => ue.UsersEventos)
-                        .ThenInclude(e => e.Evento);
-            }
-
-            query = query.AsNoTracking()
-            .OrderBy(u => u.UserName)
-                .Where(u => u.Id == UserId);
-                
-            
-            return await query.FirstOrDefaultAsync();
+            throw new System.NotImplementedException();
         }
 
-        public async Task<User[]> GetAllUsersAsyncByUserName(string userName, bool IncludeEventos = false)
+        public Task<User[]> GetAllUsersAsyncByUserName(string userName, bool IncludeEventos)
         {
-            IQueryable<User> query = _context.Users
-                .Include(c => c.RedesSociais);
-
-            if(IncludeEventos)
-            {
-                query = query
-                        .Include(pe => pe.UsersEventos)
-                        .ThenInclude(e => e.Evento);
-            }
-
-            query = query.OrderBy(u => u.UserName.ToLower().Contains(userName.ToLower()));
-                
-                
-            
-            return await query.ToArrayAsync();
+            throw new System.NotImplementedException();
         }
+
+        
+
+        
+        /*
+private readonly ProAgilContext _context;
+
+public ProAgilRepositorio(ProAgilContext context)
+{
+   _context = context;
+   _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+}
+
+//Geral
+public void Add<T>(T entity) where T : class
+{
+   _context.Add(entity);
+}
+public void Update<T>(T entity) where T : class
+{
+   _context.Update(entity);
+}
+public void Delete<T>(T entity) where T : class
+{
+   _context.Remove(entity);
+}
+public void DeleteRange<T>(T[] entityArray) where T : class
+{
+   _context.RemoveRange(entityArray);
+}
+public async Task<bool> SaveChangesAsync()
+{
+   return (await _context.SaveChangesAsync()) > 0;
+}
+
+//Evento
+public async Task<Evento[]> GetAllEventoAsync(bool IncludeUsers = false)
+{
+   IQueryable<Evento> query = _context.Eventos
+       .Include(c => c.Lotes)
+       .Include(c => c.RedesSociais);
+
+   if (IncludeUsers)
+   {
+       query = query
+           .Include(ue => ue.UsersEventos)
+           .ThenInclude(u => u.User);
+   }
+
+   query = query.AsNoTracking()
+               .OrderBy(c => c.Id);
+
+   return await query.ToArrayAsync();
+}
+
+public async Task<Evento> GetEventoAsyncById(int EventoId, bool IncludeUsers)
+{
+  IQueryable<Evento> query = _context.Eventos
+       .Include(c => c.Lotes)
+       .Include(c => c.RedesSociais);
+
+   if(IncludeUsers)
+   {
+       query = query
+               .Include(ue => ue.UsersEventos)
+               .ThenInclude(u => u.User);
+   }
+
+   query = query.AsNoTracking()
+   .OrderBy(c => c.Id)
+       .Where(c => c.Id == EventoId);
+
+   return await query.FirstOrDefaultAsync();
+}
+
+public async Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includeUsers)
+{
+   IQueryable<Evento> query = _context.Eventos
+       .Include(c => c.Lotes)
+       .Include(c => c.RedesSociais);
+
+   if (includeUsers)
+   {
+       query = query
+           .Include(ue => ue.UsersEventos)
+           .ThenInclude(p => p.User);
+   }
+
+   query = query.AsNoTracking()
+               .OrderByDescending(c => c.DataEvento)
+               .Where(c => c.Tema.ToLower().Contains(tema.ToLower()));
+
+   return await query.ToArrayAsync();
+}
+
+//Palestrante
+public async Task<User> GetAllUserAsync(int UserId, bool IncludeEventos = false)
+{
+   IQueryable<User> query = _context.Users
+       .Include(c => c.RedesSociais);
+
+   if(IncludeEventos)
+   {
+       query = query
+               .Include(ue => ue.UsersEventos)
+               .ThenInclude(e => e.Evento);
+   }
+
+   query = query.AsNoTracking()
+   .OrderBy(u => u.UserName)
+       .Where(u => u.Id == UserId);
+
+
+   return await query.FirstOrDefaultAsync();
+}
+
+public async Task<User[]> GetAllUsersAsyncByUserName(string userName, bool IncludeEventos = false)
+{
+   IQueryable<User> query = _context.Users
+       .Include(c => c.RedesSociais);
+
+   if(IncludeEventos)
+   {
+       query = query
+               .Include(pe => pe.UsersEventos)
+               .ThenInclude(e => e.Evento);
+   }
+
+   query = query.OrderBy(u => u.UserName.ToLower().Contains(userName.ToLower()));
+
+
+
+   return await query.ToArrayAsync();
+}*/
     }
 }
